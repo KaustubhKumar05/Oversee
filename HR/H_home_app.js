b404722a38b7698a47c39f7ecbc0c475bd23b043
KaustubhKumar <kaustubhknairme@gmail.com>
2020-11-27 21:21:13 +0530

    Sticky nav. Can add new items in project section

--- HR/H_home_app.js
+++ HR/H_home_app.js
@@ -1,17 +1,32 @@
 const panel = document.getElementsByClassName("panel")[0];
 const modal = document.getElementsByClassName("modal")[0];
 const close = document.getElementById("closeModal");
+const add = document.getElementById("add");
+const varBtn = document.getElementById("addUpdate");
+const remove = document.getElementById("remove");
 
-
-document.addEventListener('click', (e)=>{
-    if(e.target.id == 'closeBtn')
+document.addEventListener('click', (e) => {
+    if (e.target.id == 'closeBtn')
         panel.classList.remove('view');
-    else if(e.target.id == 'menuBtn')
+    else if (e.target.id == 'menuBtn')
         panel.classList.add('view');
-    else if(e.target.id.toString().includes("row"))
+    else if (e.target.id.toString().includes("row")) {
+        varBtn.innerHTML = "Update";
         modal.classList.add("modal-view");
+        remove.style.display = "inline";
+    }
 })
 
-close.addEventListener('click', ()=>{
+close.addEventListener('click', () => {
     modal.classList.remove("modal-view");
 })
+
+add.addEventListener('click', () => {
+    addItem()
+})
+
+const addItem = () => {
+    varBtn.innerHTML = "Add";
+    remove.style.display = "none";
+    modal.classList.add("modal-view")
+}
