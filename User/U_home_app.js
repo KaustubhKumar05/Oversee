d33e1296183012414cd32e132cb8d00de6927d97
KaustubhKumar <kaustubhknairme@gmail.com>
2020-12-09 20:34:16 +0530

    User profile completed, leave and complaint files init

---
+++ User/U_home_app.js
@@ -0,0 +1,8 @@
+const panel = document.getElementsByClassName("panel")[0];
+
+document.addEventListener('click', (e) => {
+    if (e.target.id == 'closeBtn')
+        panel.classList.remove('view');
+    else if (e.target.id == 'menuBtn')
+        panel.classList.add('view');
+})
